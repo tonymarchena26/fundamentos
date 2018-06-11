c9aa9073b19c98420c2d5b5addba3c0a85b1850b
tonymarchena26 <39474827+tonymarchena26@users.noreply.github.com>
2018-06-11 13:48:24 -0600

    Cuadrado

---
+++ Semana 5 cuadrado/src/semana/pkg5/cuadrado/Semana5Cuadrado.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semana.pkg5.cuadrado;
+
+/**
+ *
+ * @author ulacit
+ */
+public class Semana5Cuadrado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i;
+    for (i = 1;i<= 5;i++){ 
+            int j;
+       for(j= 1; j<=5;j++){ 
+           System.out.print("*"); 
+} 
+           System.out.print("\n"); 
+    }
+    
+}
+}
