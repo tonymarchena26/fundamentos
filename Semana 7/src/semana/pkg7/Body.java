e4263661306ad40d27e9499c3a90611127d27db4
tonymarchena26 <39474827+tonymarchena26@users.noreply.github.com>
2018-06-25 13:46:12 -0600

    Semana 7

---
+++ Semana 7/src/semana/pkg7/Body.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semana.pkg7;
+
+/**
+ *
+ * @author ulacit
+ */
+public class Body {
+    private int large;
+    private int doors;
+    
+    public Body (int largeA, int doorsB){
+        large = largeA;
+        doors = doorsB;
+    }//pbody
+    
+   public void first(){
+       large = large -3;
+       System.out.print(" ");
+       
+       for (int i = 1; 1 <= large; i++){
+           
+       }//for
+   }//pfirst
+    
+}//body
---
+++ Semana 7/src/semana/pkg7/Semana7.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semana.pkg7;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ulacit
+ */
+public class Semana7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+       
+       Body body = Body (scan.nextInt(), scan.nextInt());
+    }
+
+    private static Body Body(int nextInt, int nextInt0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
